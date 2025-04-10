558b675931210b66f96e39fd634a54b1872e24cf
Andrew Clark <clarkat@hendrix.edu>
2025-04-10 16:43:01 -0500

    Implemented Atmospheric Hub #2

---
+++ First Prototype/Assets/Scripts/ToStage1.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ToStage1 : MonoBehaviour
+{
+
+    public void ReactToClick() {
+        Debug.Log("I've been clicked");
+        Initiate.Fade("Stage1", Color.black, 1.0f);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
