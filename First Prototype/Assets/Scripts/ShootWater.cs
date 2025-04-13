a04b0bc46737bfdc063388ff243ac03ba30a591c
Leigh <johnstonlh@hendrix.edu>
2025-04-13 10:13:16 -0500

    "water" shoots but doesn't aim
    Addforce isnt adding any force lol, idk why

--- First Prototype/Assets/Scripts/ShootWater.cs
+++ First Prototype/Assets/Scripts/ShootWater.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class ShootWater : MonoBehaviour
 {
+    public GameObject waterShot;
+
+    public Rigidbody2D waterBody;
+    private Rigidbody2D player;
+
+    public int moveForce;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = GetComponent<Rigidbody2D>();
+        //waterBody = waterShot.GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+         if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Shooting");
+            Shoot();
+        }
         
+    }
+
+    void Shoot(){
+        Vector2 spawn = player.transform.position;
+        Vector2 goal = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Instantiate(waterShot, spawn, Quaternion.identity);
+        Debug.Log("Adding Force: " + (goal * moveForce));
+        waterBody.AddForce(goal * moveForce);
+        Debug.Log("Velocity: " + waterBody.linearVelocity);
+
     }
 }
