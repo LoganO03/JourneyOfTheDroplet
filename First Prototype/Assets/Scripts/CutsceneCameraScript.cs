999f31f4678c666041b6c46279686dd5239e350e
JoryFulme1917 <fulmerjory@gmail.com>
2025-04-16 23:15:26 -0500

    cutscene camera working, Now on to cinematography
    As the title says

--- First Prototype/Assets/Scripts/CutsceneCameraScript.cs
+++ First Prototype/Assets/Scripts/CutsceneCameraScript.cs
@@ -1,32 +1,27 @@
 // Borrowed from Jauss and Leigh's Belonging (Thank you so much friends)
 using UnityEngine;
 
-public class CutsceneCameraScript : MonoBehaviour
+public class CutsceneCameraScriptY : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float destinationY;       // Target Y position
+    public float slurpSpeed;         // How quickly the camera accelerates
+    public float maxSpeed;           // Maximum camera speed
 
-    public float destination;
-    public float slurpSpeed;
-    public float maxSpeed;
-    
+    public GameObject background0;   // Background object to move with parallax
+    public float speed0;             // Background parallax speed factor
 
-    public GameObject background0;
-    public float speed0;
-
-    float curSlurp;
-
+    float curSlurp;                  // Internal smoothing factor
 
     void Start()
     {
         curSlurp = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         curSlurp = curSlurp + (slurpSpeed - curSlurp) * slurpSpeed;
-        float cameraSpeed = Mathf.Min(maxSpeed, (destination - this.transform.position.y) * curSlurp) * Time.deltaTime;
-        this.transform.position = new Vector3(this.transform.position.x + cameraSpeed, this.transform.position.y, this.transform.position.z);
-        background0.transform.position = new Vector3(background0.transform.position.x + cameraSpeed * speed0, background0.transform.position.y, background0.transform.position.z);
+        float cameraSpeed = Mathf.Min(maxSpeed, (destinationY - this.transform.position.y) * curSlurp) * Time.deltaTime;
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + cameraSpeed, this.transform.position.z);
+        background0.transform.position = new Vector3(background0.transform.position.x,background0.transform.position.y + cameraSpeed * speed0, background0.transform.position.z);
     }
 }
