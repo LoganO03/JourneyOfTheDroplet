be34e6f35182ba9daa5fefa9262159dbc37c0f21
Jauss <50054870+IsaacLizardKing@users.noreply.github.com>
2025-04-16 21:40:23 -0500

    Counters hit their targets now!
    I nerd sniped myself so hard. I felt like a baby programmer for a second there

--- First Prototype/Assets/Scripts/Counter Scripts/CounterArray.cs
+++ First Prototype/Assets/Scripts/Counter Scripts/CounterArray.cs
@@ -4,32 +4,38 @@ public class CounterArray : MonoBehaviour
 {
     [SerializeField] CounterDigit[] digits;
     [SerializeField] public float goalNumber;
-    [SerializeField] float slurpSpeed;
-    [SerializeField] float rotationSpeed;
+    float prevGoal = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        /*
         int i = digits.Length - 1;
         while (i >= 0) {
-            digits[i].rotationSpeed = rotationSpeed;
-            digits[i].slurpSpeed = slurpSpeed;
             i -= 1;
         }
         i = (int) Mathf.Log(goalNumber, 10);
         while (i >= 0) {
             i -= 1;
-        }
+        }*/
     }
 
     // Update is called once per frame
     void Update() {
         //Check what number we're at right now
-        int i = digits.Length - 1;
+        /*int i = digits.Length - 1;
         float total = 0;
         while(i >= 0) {
             total = total * 10 + digits[i].at();
             i -= 1;
+        }*/
+        if(prevGoal != goalNumber) {
+            float num = digits[0].at();
+            digits[0].newGoal(goalNumber - num, goalNumber);
         }
+        prevGoal = goalNumber;
+
+
+
 
         //Debug.Log(total);
 
--- First Prototype/Assets/Scripts/Counter Scripts/CounterDigit.cs
+++ First Prototype/Assets/Scripts/Counter Scripts/CounterDigit.cs
@@ -5,11 +5,10 @@ public class CounterDigit : MonoBehaviour
     [SerializeField] CounterCube counter;
     [SerializeField] Transform wheel;
     [SerializeField] int digitIndex;
-    public float rotationSpeed;
-    public float slurpSpeed;
-    public float goalNumber = 0;
-    public float rotations = 0;
+    float goalNumber = 0;
+    float rotations = 0;
     float currentNumber = 0;
+    [SerializeField] float maxDegreeDiff = 150;
     
     float currentSlurp = 0;
     float currentSpeed = 0;
@@ -18,34 +17,30 @@ public class CounterDigit : MonoBehaviour
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        
+        //maxDegreeDiff *= Mathf.PI / 180;
     }
     
     void Awake()
     {
         currentNumber = counter.displayedNum();
         if(Mathf.Pow(10, digitIndex) > goalNumber){
-            counter.disappear();
+            //counter.disappear();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(rotations != 0){
-            if(currentNumber - counter.displayedNum() > 9){
-                currentNumber -= 10;
-            } else if(counter.displayedNum() - currentNumber > 9){
-                currentNumber += 10;
-            } else {
-                rotations -= (currentNumber - counter.displayedNum());
-            }
+        if(counter.displayedNum() > 7 && currentNumber < 2) {
+            rotations -= (counter.displayedNum() - (currentNumber + 10));
+        } else if (counter.displayedNum() < 2 && 7 < currentNumber) {
+            rotations -= ((counter.displayedNum() + 10) - currentNumber);
+        } else {
+            rotations -= (counter.displayedNum() - currentNumber);
         }
         currentNumber = counter.displayedNum();
-        if(currentNumber < goalNumber) {
-            increment();
-        } else if (currentNumber > goalNumber) {
-            decrement();
+        if (Mathf.Abs(rotations) > 1.5) {
+            rotate();
         } else {
             stick();
         }
@@ -53,39 +48,121 @@ public class CounterDigit : MonoBehaviour
 
     public float at() {
         if(digitIndex != 0) {
-            return (int) (currentNumber + 0.1f) % 10;
+            return (int) (counter.displayedNum() + 0.1f) % 10;
         }
-        return currentNumber;
+        return counter.displayedNum();
     }
 
-    void increment() {
+    public Vector2 floatToComponents(float a) {
+        a = a % 1;
+        return new Vector2(Mathf.Cos((Mathf.PI / 2) * a), Mathf.Sin((Mathf.PI / 2) * a));
+    }
 
-        if(accelFlag != 1) {
-            currentSlurp = 0;
-            accelFlag = 1;
-            currentSpeed = 0;
-        } else {
-            currentSlurp += (slurpSpeed - currentSlurp) * slurpSpeed;
-            currentSpeed += (rotationSpeed - currentSpeed) * currentSlurp;
-        }
-        float angle = currentSpeed * Time.deltaTime / Mathf.Pow(10, digitIndex);
-        wheel.transform.rotation *= Quaternion.AngleAxis(angle, Vector3.right);
+    public void newGoal(float rot, float goal) {
+        goalNumber = goal;
+        rotations = rot;
     }
     
-    void decrement() {
-        if(accelFlag != -1) {
-            currentSlurp = 0;
-            currentSpeed = 0;
-            accelFlag = -1;
-        } else {
-            currentSlurp += (slurpSpeed - currentSlurp) * slurpSpeed;
-            currentSpeed += (rotationSpeed - currentSpeed) * currentSlurp;
-        }
-        float angle = -currentSpeed * Time.deltaTime / Mathf.Pow(10, digitIndex);
-        wheel.transform.rotation *= Quaternion.AngleAxis(angle, Vector3.right);
-    }
 
     void stick() {
 
+        float y;
+        float z;
+
+        // determine if a is y or z
+        { Vector2 Temp = floatToComponents(goalNumber);
+          if(((int) goalNumber) % 2 == 0) {
+            y = Temp.x;
+            z = Temp.y;
+          } else {
+            y = Temp.y;
+            z = Temp.x;
+          }
+        }
+        Debug.Log($"raw vector: {y}, {z}");
+
+        // determine signs of y and z
+            // determine the placement of the counter on the 5 spin chart
+        float countery = counter.transformPweaseOwO().up.y;
+        float counterz = counter.transformPweaseOwO().up.z;
+        float c = counter.displayedNum();
+        float tempGoal = goalNumber;
+        if(goalNumber < 1 && c > 8) {
+            goalNumber += 10;
+        }
+        if(goalNumber > 8 && c < 1) {
+            goalNumber -= 10;
+        }
+        bool invert = false;
+        if(c == c - (c % 1)) {
+            if(c == 0 || c == 4 || c == 8) {
+                invert = countery > 0;
+            }
+            if(c == 1 || c == 5 || c == 9) {
+                invert = counterz > 0;
+            } 
+            if(c == 2 || c == 6) {
+                invert = countery < 0;
+            }
+            if(c == 3 || c == 7) {
+                invert = counterz < 0;
+            }
+        } else {
+            if(c < 1) {
+                invert = counterz > 0;
+            }
+            if((1 < c && c < 3)) {
+                invert = countery < 0;
+            }
+            
+            if(3 < c && c < 5) {
+                invert = countery > 0;
+            }
+
+            if(5 < c && c < 7) {
+                invert = countery < 0;
+            }
+
+            if(7 < c && c < 9) {
+                invert = countery > 0;
+            }
+
+            if (9 < c) {
+                invert = countery < 0;
+            }
+        }
+
+
+        if((goalNumber > 1 && goalNumber < 3) || (goalNumber > 5 && goalNumber < 7) || goalNumber > 9) {
+            y = -y;
+        }
+        if((goalNumber > 2 && goalNumber < 4) || (goalNumber > 6 && goalNumber < 8)) {
+            z = -z;
+        }
+        if(!invert){
+            y = -y;
+            z = -z;
+        }
+        Debug.Log($"refined vector: {y}, {z}");
+        wheel.transform.LookAt(wheel.transform.position + new Vector3(0, y, z));    
+        Debug.Log(wheel.transform.rotation.eulerAngles);
+        if(wheel.transform.rotation.eulerAngles.z != 0) {
+            wheel.transform.rotation *= Quaternion.AngleAxis(-wheel.transform.rotation.eulerAngles.z, Vector3.forward);
+            wheel.transform.rotation *= Quaternion.AngleAxis(180, Vector3.right);
+        }
+        if(wheel.transform.rotation.eulerAngles.y != 0) {
+            wheel.transform.rotation *= Quaternion.AngleAxis(-wheel.transform.rotation.eulerAngles.y, Vector3.up);
+            wheel.transform.rotation *= Quaternion.AngleAxis(180, Vector3.right);
+        }
+        
+        wheel.transform.rotation *= Quaternion.AngleAxis(90, Vector3.right);
+        goalNumber = tempGoal;
+    }
+
+    void rotate() {
+        // get the counter's rotation
+        // set the rotation to maxDiff / 10^digitIndex 
+        float angle = maxDegreeDiff / Mathf.Pow(10, digitIndex) * Mathf.Sign(rotations);
+        wheel.transform.rotation = counter.transformPweaseOwO().rotation * Quaternion.AngleAxis(angle, Vector3.right);
     }
 }
