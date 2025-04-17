0cd52479524b9006f9b307ba052bbfa34ecf72c4
JoryFulme1917 <fulmerjory@gmail.com>
2025-04-16 22:06:14 -0500

    made alterations to camera for cutscene
    moving to perfecting the cutscene

--- First Prototype/Assets/Scripts/CutsceneCameraScript.cs
+++ First Prototype/Assets/Scripts/CutsceneCameraScript.cs
@@ -1,7 +1,7 @@
 // Borrowed from Jauss and Leigh's Belonging (Thank you so much friends)
 using UnityEngine;
 
-public class StartMenuScript : MonoBehaviour
+public class CutsceneCameraScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
@@ -9,14 +9,7 @@ public class StartMenuScript : MonoBehaviour
     public float slurpSpeed;
     public float maxSpeed;
     
-    public GameObject background4;
-    public float speed4;
-    public GameObject background3;
-    public float speed3;
-    public GameObject background2;
-    public float speed2;
-    public GameObject background1;
-    public float speed1;
+
     public GameObject background0;
     public float speed0;
 
@@ -32,12 +25,8 @@ public class StartMenuScript : MonoBehaviour
     void Update()
     {
         curSlurp = curSlurp + (slurpSpeed - curSlurp) * slurpSpeed;
-        float cameraSpeed = Mathf.Min(maxSpeed, (destination - this.transform.position.x) * curSlurp) * Time.deltaTime;
+        float cameraSpeed = Mathf.Min(maxSpeed, (destination - this.transform.position.y) * curSlurp) * Time.deltaTime;
         this.transform.position = new Vector3(this.transform.position.x + cameraSpeed, this.transform.position.y, this.transform.position.z);
         background0.transform.position = new Vector3(background0.transform.position.x + cameraSpeed * speed0, background0.transform.position.y, background0.transform.position.z);
-        background1.transform.position = new Vector3(background1.transform.position.x + cameraSpeed * speed1, background1.transform.position.y, background1.transform.position.z);
-        background2.transform.position = new Vector3(background2.transform.position.x + cameraSpeed * speed2, background2.transform.position.y, background2.transform.position.z);
-        background3.transform.position = new Vector3(background3.transform.position.x + cameraSpeed * speed3, background3.transform.position.y, background3.transform.position.z);
-        background4.transform.position = new Vector3(background4.transform.position.x + cameraSpeed * speed4, background4.transform.position.y, background4.transform.position.z);
     }
 }
