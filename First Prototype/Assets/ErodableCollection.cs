a704993498f19a3575b28b4713fa3e964f76edb0
Jauss <50054870+IsaacLizardKing@users.noreply.github.com>
2025-04-22 08:13:31 -0500

    Made a sand castle
    making a potential final prototype for the erosion engine

---
+++ First Prototype/Assets/ErodableCollection.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ErodableCollection : MonoBehaviour
+{
+    [SerializeField] public Texture2D Rocks; // visual texture
+    [SerializeField] public Texture2D Voronoi; // rock lookup map
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
