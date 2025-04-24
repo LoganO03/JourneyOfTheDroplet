58b3af20922efcd52b6e2f4d3b118093891834a9
Andrew Clark <clarkat@hendrix.edu>
2025-04-23 21:11:10 -0500

    Implemented movement into mini-game #20

---
+++ First Prototype/Assets/Scripts/CanvasFade.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CanvasFade : MonoBehaviour
+{
+    [SerializeField] CanvasGroup Fade;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("FadeOut", 5f);
+    }
+
+    void FadeOut(){
+        Fade.alpha -= Time.deltaTime;
+    }
+}
