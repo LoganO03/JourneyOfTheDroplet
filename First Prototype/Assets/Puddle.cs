7956834d7797ebbd056235c7a6bfcf1b35f8cfda
Jauss <50054870+IsaacLizardKing@users.noreply.github.com>
2025-04-30 21:24:05 -0500

    adjusted water gained from stalactite drops
    also remove a bunch of debug.log statements. It was clogging up the terminal

--- First Prototype/Assets/Puddle.cs
+++ First Prototype/Assets/Puddle.cs
@@ -23,20 +23,16 @@ public class Puddle : MonoBehaviour
             float suckWater = (transform.localScale.x * transform.localScale.y) * suckSpeed;
             GameManager.Instance.playerWater += suckWater;
             transform.localPosition -= Vector3.up * suckWater / transform.localScale.z;
-            Debug.Log(other.gameObject.name);
         }
         if(other.gameObject.layer == 8 && other.transform.localScale.x > 0.01f) { // pickup layer
             float suckSpeed = 2 * Time.deltaTime / other.transform.localScale.z; //other.transform.localScale.x / transform.localScale.x;
             float suckWater = suckSpeed / transform.localScale.z;
-            Debug.Log($"{suckWater} + {transform.localPosition.y} = {suckWater + transform.localPosition.y}\nis it less than {fullHeight.y}?");
             transform.localPosition += Vector3.up * (suckWater);
             other.transform.localScale -= new Vector3(other.transform.localScale.x, other.transform.localScale.y, 0) * (suckWater / (other.transform.localScale.x * other.transform.localScale.y * other.transform.localScale.z));
-            Debug.Log(other.gameObject.name);
         }
         if(other.gameObject.layer == 4) { // water projectile layer
             float otherVolume = (other.transform.localScale.x * other.transform.localScale.y) * Mathf.PI * other.transform.localScale.z;
             transform.localPosition += Vector3.up * otherVolume / (transform.localScale.x * transform.localScale.y * transform.localScale.z);
-            Debug.Log(other.name);
         }
     }
 
@@ -44,15 +40,12 @@ public class Puddle : MonoBehaviour
         if(other.gameObject.layer == 8) { // pickup layer
             float suckSpeed = 2 * Time.deltaTime / other.transform.localScale.z; //other.transform.localScale.x / transform.localScale.x;
             float suckWater = suckSpeed / transform.localScale.z;
-            Debug.Log($"{suckWater} + {transform.localPosition.y} = {suckWater + transform.localPosition.y}\nis it less than {fullHeight.y}?");
             transform.localPosition += Vector3.up * (suckWater);
             other.transform.localScale -= new Vector3(other.transform.localScale.x, other.transform.localScale.y, 0) * (suckWater / (other.transform.localScale.x * other.transform.localScale.y * other.transform.localScale.z));
-            Debug.Log(other.gameObject.name);
         }
         if(other.gameObject.layer == 4) { // water projectile layer
             float otherVolume = (other.transform.localScale.x * other.transform.localScale.y) * Mathf.PI * other.transform.localScale.z;
             transform.localPosition += Vector3.up * otherVolume / (transform.localScale.x * transform.localScale.y * transform.localScale.z);
-            Debug.Log(other.gameObject.name);
         }
     }
 }
