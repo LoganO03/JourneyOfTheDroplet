8947ea1ca8f99caf0a507ce275ac79487d054506
EdenRobbins-Hendrix <eerobbins82@gmail.com>
2025-10-15 14:12:12 -0500

    More fixes

--- First Prototype/Assets/Scripts/ToStage1.cs
+++ First Prototype/Assets/Scripts/ToStage1.cs
@@ -18,8 +18,9 @@ public class ToStage1 : MonoBehaviour
     public void ToStageTwo()
     {
         Initiate.Fade("Stage2", Color.black, 1.0f);
+
         AudioInbetween.Instance.swapTrack(2);
-        AudioInbetween.Instance.GetComponent<AudioSource>().mute = false;
+        AudioInbetween.Instance.GetComponent<AudioSource>().Stop();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
